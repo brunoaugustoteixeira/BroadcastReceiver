fea5507bd821839d7c5def31a9d55db17526af30
bruno.teixeira <brunoaugustoteixeira@igti.com.br>
2020-08-11 21:24:58 -0300

    first commit

---
+++ app/src/main/java/com/example/broadcastreceiver/MyReceiver.java
@@ -0,0 +1,20 @@
+package com.example.broadcastreceiver;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
+
+public class MyReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+        if(intent.ACTION_AIRPLANE_MODE_CHANGED.equals(intent.getAction())) {
+            Toast.makeText(context, "Broadcast Recebido Airplane", Toast.LENGTH_LONG).show();
+        }
+        if(intent.ACTION_BATTERY_LOW.equals(intent.getAction())){
+            Toast.makeText(context, "Broadcast Recebido Bateria", Toast.LENGTH_LONG).show();
+        }
+    }
+}
